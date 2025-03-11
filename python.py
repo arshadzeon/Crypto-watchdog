6a3fed645ea492f8407261ca0f945e562a307134
name <arshadalib007@gmail.com>
2025-03-11 21:46:06 +0530

    did data-preprocessing

--- python.py
+++ python.py
@@ -11,6 +11,23 @@ import seaborn as sns
 
 data = pd.read_csv('transaction_dataset.csv')
 
-imputer = SimpleImputer( strategy='constant' , fill_value = 2)
+X = data.drop(['FLAG', 'index','address' ], axis=1)  
+y = data['FLAG']  
 
-X_train , X_test , y_train , y_test = train_test_split(X , y , test_size= 0.3 , random_state=42 )
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+imputer = SimpleImputer( strategy='mean')
+X_train = imputer.fit_transform(X_train)
+X_test = imputer.transform(X_test)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+smote = SMOTE(random_state=42)
+X_train, y_train = smote.fit_resample(X_train, y_train)
+
+encoder = LabelEncoder()
+y_train = encoder.fit_transform(y_train)
+y_test = encoder.transform(y_test)
+
