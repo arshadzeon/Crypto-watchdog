d52b1a9dde643b981389159043ba128052bc7369
name <arshadalib007@gmail.com>
2025-03-13 23:46:32 +0530

    hyperparameter tuning

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-import pandas as pd 
-from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import  LabelEncoder , OneHotEncoder
-from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import accuracy_score
-
+from sklearn.pipeline import Pipeline
 
 data = pd.read_csv('transaction_dataset.csv')
 
-X = data.drop(['FLAG', 'Index','Address' ], axis=1)  
-y = data['FLAG']  
+X = data.drop(['FLAG', 'Index', 'Address'], axis=1)
+y = data['FLAG']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-numerical_cols = X_train.select_dtypes(include =['float64', 'int64']).columns
+numerical_cols = X_train.select_dtypes(include=['float64', 'int64']).columns
 categorical_cols = X_train.select_dtypes(include=['object']).columns
 
 preprocessor = ColumnTransformer(
-   transformers= [
-       ('nums' , SimpleImputer(strategy='mean'), numerical_cols ),
-       ('cats', OneHotEncoder(handle_unknown='ignore'), categorical_cols )
-   ]
+    transformers=[
+        ('nums', Pipeline([
+            ('imputer', SimpleImputer(strategy='mean')),
+            ('scaler', StandardScaler())
+        ]), numerical_cols),
+        ('cats', OneHotEncoder(handle_unknown='ignore'), categorical_cols)
+    ]
 )
 
 X_train = preprocessor.fit_transform(X_train)
@@ -37,19 +39,18 @@ y_train = encoder.fit_transform(y_train)
 y_test = encoder.transform(y_test)
 
 param_grid = {
-'C' : [1],
-'kernel' : ['rbf'],
-'gamma' : ['scale']
+    'C': [0.1, 1, 10, 100],
+    'kernel': ['linear', 'rbf', 'poly'],
+    'gamma': ['scale', 'auto', 0.01, 0.1, 1],
+    'degree': [2, 3, 4]
 }
 
 svc = SVC()
-grid_search = GridSearchCV( svc , param_grid , cv=5 , scoring='accuracy')
-grid_search.fit(X_train , y_train)
+grid_search = GridSearchCV(svc, param_grid, cv=5, scoring='accuracy')
+grid_search.fit(X_train, y_train)
 
 finalmodel = grid_search.best_estimator_
-
 y_pred = finalmodel.predict(X_test)
 
+print("Best Parameters:", grid_search.best_params_)
 print("Accuracy:", accuracy_score(y_test, y_pred))
-
-
